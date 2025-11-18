9dd789bb5bfd0e40b32ba70062f95348381b3030
stefano-berti <stefano@rendr.io>
2025-11-18 20:46:24 +0100

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-'use client'
+"use client";
 
-import { useActionState } from 'react'
-import { subscribeAction, type SubscribeState } from './actions/subscribe'
-import { ThankYou } from './components/ThankYou'
-import { RegistrationForm } from './components/RegistrationForm'
+import { useActionState } from "react";
+import { subscribeAction, type SubscribeState } from "./actions/subscribe";
+import { ThankYou } from "./components/ThankYou";
+import { RegistrationForm } from "./components/RegistrationForm";
 
 export default function Home() {
-  const [state, formAction, isPending] = useActionState<SubscribeState | null, FormData>(
-    subscribeAction,
-    null
-  )
+  const [state, formAction, isPending] = useActionState<
+    SubscribeState | null,
+    FormData
+  >(subscribeAction, null);
 
   if (state?.success) {
-    return <ThankYou message={state.message} />
+    return <ThankYou message={state.message} />;
   }
 
   return (
@@ -21,5 +21,5 @@ export default function Home() {
       state={state}
       isPending={isPending}
     />
-  )
+  );
 }
